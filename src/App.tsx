f42700c9c0abceea295b3bdd79889d75f817c954
fake js developer <112frosta@proton.me>
2025-01-04 18:45:07 +0100

    good

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 export default function App() {
   return (
-    <div className="h-screen bg-slate-900 text-rose-600 flex justify-center items-center pb-32">
-      <div className="text-6xl font-black text-center leading-normal">
-        KUP TANIOMEFEDRON JUZ DZISIAJ <br />
-        <span className="underline">discord: taniomefa</span>!!!!
+    <div className="min-h-screen h-screen">
+      <div className="container h-full mx-auto p-5">
+        <div className="h-full flex-col space-y-10 flex pb-32 justify-center items-center">
+          <div className="space-y-2 text-center">
+            <h1 className="text-3xl md:text-5xl font-extrabold text-center md:leading-snug leading-tight">
+              Chcesz kupic mefedron?
+            </h1>
+            <p className="text-blue-600 font-bold">
+              discord: <span className="underline">taniomefa</span>
+            </p>
+          </div>
+          <div>
+            <img src="./mefa.jpeg" className="size-64 object-contain" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
